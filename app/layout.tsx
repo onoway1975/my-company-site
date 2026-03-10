79c179d62faf2aad6f18ee7b3e21cfd6a5054e3c
onoway <onoe@ciraf.jp>
2026-03-10 16:22:25 +0900

    GTMをafterInteractiveに変更してパフォーマンス改善

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import localFont from "next/font/local";
+import Script from "next/script";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
 import { ChatWidget } from "./components/ChatWidget";
@@ -107,16 +108,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja" className={lineSeed.variable}>
-      <head>
+      <body className="font-sans">
         {/* Google Tag Manager */}
-        <script
+        <Script
+          id="gtm"
+          strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-N85P87G');`,
           }}
         />
-        {/* End Google Tag Manager */}
-      </head>
-      <body className="font-sans">
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
@@ -126,7 +126,7 @@ export default function RootLayout({
             style={{ display: "none", visibility: "hidden" }}
           />
         </noscript>
-        {/* End Google Tag Manager (noscript) */}
+        {/* End Google Tag Manager */}
         <Header />
         <main className="pt-16">{children}</main>
         <Footer />
